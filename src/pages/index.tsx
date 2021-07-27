c40f20296bc62a406b0713ffac1980f1a98468e0
Maxwell Rayman <maxwellrayman@maxwells-mbp.myfiosgateway.com>
2021-07-27 13:54:44 -0400

    added media query for banner and HBG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Image } from "@chakra-ui/react";
+import { Flex, Box, Text, Image, useMediaQuery } from "@chakra-ui/react";
 import { memberList } from "../data/memberList";
 import MediaButtons from "../components/MediaButtons";
 import axios from "axios";
@@ -25,99 +25,111 @@ const Index = ({ liveData }) => {
     return streamData.user_login;
   });
 
+  const [isSmallerThan768] = useMediaQuery("(max-width: 768px)");
+  const [isSmallerThan480] = useMediaQuery("(max-width: 480px)");
+
   return (
     <>
-      <Box height="5em" backgroundImage="url(/img/dirt4.jpg)"></Box>
-      <Flex backgroundImage="url(/img/dirt3.jpg)">
-        <Box
-          m="auto"
-          width="95%"
-          p="22px"
-          zIndex="0"
-          borderRadius="8px"
-          textAlign="center"
-        >
-          <Image
-            borderRadius="8px"
-            border="2px solid black"
-            src="/img/hbg_banner.jpeg"
-            margin="auto"
-          />
-          <Image
+      <Box>
+        <Box height="5em" backgroundImage="url(/img/dirt4.jpg)"></Box>
+        <Flex backgroundImage="url(/img/dirt3.jpg)">
+          <Box
             m="auto"
-            mt="3em"
-            pb="1.5em"
-            src="/img/HOUSE_BUILDER_GANG.png"
-          />
-          <Box>
-            <FaTwitter
-              onClick={() => {
-                window.open("https://twitter.com/hbg_mc");
-              }}
-              cursor="pointer"
-              size={32}
-              style={{ margin: "auto" }}
-              color="#1DA1F2"
+            width="95%"
+            p="22px"
+            zIndex="0"
+            borderRadius="8px"
+            textAlign="center"
+          >
+            <Image
+              objectFit={isSmallerThan768 ? "cover" : "inherit"}
+              width="100%"
+              height={isSmallerThan768 ? "200px" : "inherit"}
+              borderRadius="8px"
+              border="2px solid black"
+              src="/img/hbg_banner.jpeg"
+              margin="auto"
             />
+            {isSmallerThan480 ? (
+              <Image m="auto" mt="3em" pb="1.5em" src="/img/HBG.png" />
+            ) : (
+              <Image
+                m="auto"
+                mt="3em"
+                pb="1.5em"
+                src="/img/HOUSE_BUILDER_GANG.png"
+              />
+            )}
+            <Box>
+              <FaTwitter
+                onClick={() => {
+                  window.open("https://twitter.com/hbg_mc");
+                }}
+                cursor="pointer"
+                size={32}
+                style={{ margin: "auto" }}
+                color="#1DA1F2"
+              />
+            </Box>
           </Box>
-        </Box>
-      </Flex>
-      <Box backgroundImage="url(/img/stone4.jpg)" pb="2em">
-        <Image m="auto" p="3em" src="/img/members.png" />
-        <Flex flexWrap="wrap" justifyContent="center">
-          {memberList.map((member) => {
-            const isLive =
-              liveChannels.findIndex(
-                (channel: any) => channel === member.twitch
-              ) >= 0;
-            return (
-              <Box key={member.name} mb="2em">
-                <Box textAlign="center">
-                  <Text fontWeight="bold" color="white">
-                    {member.name}
-                  </Text>
-                </Box>
-                <Link href={`/member/${member.name}`}>
-                  <Box
-                    cursor="pointer"
-                    textAlign="center"
-                    backgroundImage={`/img/avatars/${member.twitter}.jpg`}
-                    backgroundSize="cover"
-                    p=".5em"
-                    m="0em 1em"
-                    borderRadius="1em"
-                    border={isLive ? "2px solid red" : "1px solid lightgray"}
-                    width="8em"
-                    height="7em"
-                    _hover={{
-                      boxShadow: "-4px 4px 10px black",
-                    }}
-                  >
-                    {isLive && (
-                      <FaCircle style={{ float: "right" }} color="red" />
-                    )}
-                  </Box>
-                </Link>
-                <Box>
-                  <MediaButtons
-                    twitter={member.twitter}
-                    youtube={member.youtube}
-                    twitch={member.twitch}
-                  />
-                </Box>
-              </Box>
-            );
-          })}
         </Flex>
+        <Box backgroundImage="url(/img/stone4.jpg)" pb="2em">
+          <Image m="auto" p="3em" src="/img/members.png" />
+          <Flex m="auto" maxW="1280px" flexWrap="wrap" justifyContent="center">
+            {memberList.map((member) => {
+              const isLive =
+                liveChannels.findIndex(
+                  (channel: any) => channel === member.twitch
+                ) >= 0;
+              return (
+                <Box key={member.name} mb="2em">
+                  <Box textAlign="center">
+                    <Text fontWeight="bold" color="white">
+                      {member.name}
+                    </Text>
+                  </Box>
+                  <Link href={`/member/${member.name}`}>
+                    <Box
+                      cursor="pointer"
+                      textAlign="center"
+                      backgroundImage={`/img/avatars/${member.twitter}.jpg`}
+                      backgroundSize="cover"
+                      p=".5em"
+                      m="0em 1em"
+                      borderRadius="1em"
+                      border={isLive ? "2px solid red" : "1px solid lightgray"}
+                      width="8em"
+                      height="7em"
+                      _hover={{
+                        boxShadow: "-4px 4px 10px black",
+                      }}
+                    >
+                      {isLive && (
+                        <FaCircle style={{ float: "right" }} color="red" />
+                      )}
+                    </Box>
+                  </Link>
+                  <Box>
+                    <MediaButtons
+                      twitter={member.twitter}
+                      youtube={member.youtube}
+                      twitch={member.twitch}
+                    />
+                  </Box>
+                </Box>
+              );
+            })}
+          </Flex>
+        </Box>
+        <Box
+          as="footer"
+          mx="auto"
+          width="100%"
+          py="12"
+          px={{ base: "4", md: "8" }}
+          backgroundImage="url(/img/mossy.jpg)"
+        ></Box>
       </Box>
-      <Box
-        as="footer"
-        mx="auto"
-        width="100%"
-        py="12"
-        px={{ base: "4", md: "8" }}
-        backgroundImage="url(/img/mossy.jpg)"
-      ></Box>
     </>
   );
 };
